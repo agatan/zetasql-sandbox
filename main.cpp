b7fcf44586924bf4deff391bafaabcee03f88316
agatan <agatan039@gmail.com>
2020-05-21 09:23:32 +0900

    Refactor

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 
 #include "zetasql/public/analyzer.h"
+#include "zetasql/public/simple_catalog.h"
+
+std::string read_all(std::istream& in) {
+    std::istreambuf_iterator<char> begin(in), end;
+    return {begin, end};
+}
 
 int main() {
-    std::istreambuf_iterator<char> begin(std::cin), end;
-    const std::string query(begin, end);
+    const std::string query = read_all(std::cin);
 
     const zetasql::AnalyzerOptions analyzer_options;
     zetasql::TableNamesSet table_names_set;
 
     auto status = zetasql::ExtractTableNamesFromScript(query, analyzer_options, &table_names_set);
-    std::cout << "status: " << status.ToString() << std::endl;
+    if (!status.ok()) {
+        std::cout << "status: " << status.ToString() << std::endl;
+        return 1;
+    }
     for (std::vector<std::string> const& table_name : table_names_set) {
         size_t i = 0;
         for (const std::string& path : table_name) {
